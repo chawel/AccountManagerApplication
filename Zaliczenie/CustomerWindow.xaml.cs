3d0cc67c41486ab2c50a75ab510806ceb23b06fd
Pawel Ch <pawel.chaniewski@gmail.com>
2018-06-16 15:17:03 +0200

    Add project files.

---
+++ Zaliczenie/CustomerWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace AccountManager
+{
+	/// <summary>
+	/// Interaction logic for CustomerWindow.xaml
+	/// </summary>
+	public partial class CustomerWindow : Window
+	{
+		public CustomerWindow()
+		{
+			InitializeComponent();
+		}
+
+		private void OkButton_Click(object sender, RoutedEventArgs e)
+		{
+			if (String.IsNullOrWhiteSpace(SurnameTextBox.Text))
+			{
+				MessageBox.Show("Musisz podać jakąś wartość!", "Błędne nazwisko", MessageBoxButton.OK, MessageBoxImage.Error);
+				return;
+			}
+
+			DialogResult = true;
+		}
+
+		private void CancelButton_Click(object sender, RoutedEventArgs e)
+		{
+			DialogResult = false;
+		}
+	}
+}
---
+++ Zaliczenie/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AccountManager
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+
+			// Lokacja gdzie pojwi się główne okno ustawione na środek ekranu
+			WindowStartupLocation = WindowStartupLocation.CenterScreen;
+		}
+
+		private void AddCustomerButton_Click(object sender, RoutedEventArgs e)
+		{
+			// Aby okno pojawiło się centrum okna rodzica (czyli MainWindow)
+			// this == MainWindow
+			var customerWindow = new CustomerWindow { Owner = this, WindowStartupLocation = WindowStartupLocation.CenterOwner };
+
+			if (customerWindow.ShowDialog() == true)
+			{
+
+			}
+		}
+
+		private void AddAccountButton_Click(object sender, RoutedEventArgs e)
+		{
+			var accountWindow = new AccountWindow { Owner = this, WindowStartupLocation = WindowStartupLocation.CenterOwner };
+
+			if (accountWindow.ShowDialog() == true)
+			{
+
+			}
+		}
+	}
+}
